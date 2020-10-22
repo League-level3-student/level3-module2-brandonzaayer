7878d6793df35204bfd25589720166158767f741
brandonzaayer <33267170+brandonzaayer@users.noreply.github.com>
2020-10-21 17:28:50 -0700

    finished sorting algorithms. working on searching algorithms, the interpolation is incomplete

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -6,20 +6,41 @@ public class BogoSorter extends Sorter {
 	public BogoSorter() {
 		type = "Bogo";
 	}
-	
-	// Bogo sort is a joke sorting algorithm. It is considered the most 
+
+	// Bogo sort is a joke sorting algorithm. It is considered the most
 	// inefficient sorting algorithm while still maintaining the possibility
 	// of eventually sorting data.
-	
+
 	// It works by following these steps:
 	// STEP 1. Is the array in order?
-	//	if yes, finished; if no, go to step 2.
+	// if yes, finished; if no, go to step 2.
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
-	
-	//1. Complete the sort method using the Bogo sort algorithm. 
+
+	// 1. Complete the sort method using the Bogo sort algorithm.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-
+		boolean isSorted = false;
+		int i = 0;
+		int j = 1;
+		while (isSorted == false) {
+			Random ran1 = new Random();
+			int r1 = ran1.nextInt(array.length);
+			Random ran2 = new Random();
+			int r2 = ran2.nextInt(array.length);
+			while (ran1 == ran2) {
+				r2 = ran2.nextInt(array.length);
+			}
+			if (array[r1] > array[r2]) {
+				int temp = array[r1];
+				array[r1] = array[r2];
+				array[r2] = temp;
+				display.updateDisplay();
+			}
+			if (array[i] < array[j]) {
+				i++;
+				j++;
+			}
+		}
 	}
 }
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,18 +11,27 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[] strings = {"one", "two", "three", "four"};
+		int index = _00_LinearSearch.linearSearch(strings, "three");
+		assertEquals(index, 2);
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] nums = {1, 2, 3, 4, 5};
+		int index = _01_BinarySearch.binarySearch(nums, 1, 5, 4);
+		assertEquals(index, 3);
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[] nums = {1, 2, 3, 4, 5};
+		int index = _02_InterpolationSearch.interpolationSearch(nums, 4);
+		assertEquals(index, 3);
 	}
 	
 	@Test
